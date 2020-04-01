3f289a69bd7532de95919ad381ad022bf785c222
Mark Kuharich <kuharich@hotmail.com>
2020-04-01 14:05:19 -0700

    Initial Commit

---
+++ GitHub Searcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub Searcher
+//
+//  Created by Mark Kuharich on 4/1/20.
+//  Copyright © 2020 Mark Kuharich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
